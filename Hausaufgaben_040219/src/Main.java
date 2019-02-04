5351191280a756f1c9472bf59d75676e64460448
Vitok2207 <viktorstoppel@gmail.com>
2019-02-04 17:11:00 +0100

    Hausaufgabe_04.02.19

---
+++ Hausaufgaben_040219/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(" MARIO             WORLD    TIME");
+        System.out.println(" 000700    §*01     1-1      287");
+        System.out.println();
+        System.out.println();
+        System.out.println("☁   ☁   ☁   ☁   ☁   ☁   ☁   ☁");
+        System.out.println();
+        System.out.println();
+        System.out.println("                              ##");
+        System.out.println("                            ####");
+        System.out.println("                          ######");
+        System.out.println("                 ☺      ########");
+        System.out.println("                <‖>   ##########");
+        System.out.println("                //  ############");
+        System.out.println("                  ##############");
+        System.out.println("                ################");
+        System.out.println("       ___    ##################");
+        System.out.println("       | |  ####################");
+        System.out.println("       | |######################");
+        System.out.println("͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞");
+
+    }
+}
