06345fec19dea3f3356d0aa99552617775a98864
Vitok2207 <viktorstoppel@gmail.com>
2019-02-05 11:06:29 +0100

    Tag 2

--- Hausaufgaben_040219/src/Main.java
+++ Hausaufgaben_040219/src/Main.java
@@ -19,8 +19,7 @@ public class Main {
         // \n innerhalb eines print oder println Ausdrucks führt zu einem zusätzlichen Zeilenumbruch.
         System.out.print("Copyright by\t\t" + "\u00A9" + "Vitok2207\n");
         System.out.print("Preis:\t\t\t\t" + "199\u20ac\n");
-        System.out.print("Kontakt:\t\t\t" + "viktorstoppel\u0040gmail.com\n");
-
+        System.out.print("Kontakt:\t\t\t" + "viktorstoppel\u0040gmail.com\n\n\n");
         System.out.println("                              ##");
         System.out.println("                            ####");
         // Mehrere Strings können innerhalb einer Ausgabe mit einem Pluszeichen (+) verbunden werden.
