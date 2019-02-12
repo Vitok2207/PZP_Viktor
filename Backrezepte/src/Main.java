50aeee2b5cd62c4c8102eaf8cdfa20fafb859cb0
Vitok2207 <viktorstoppel@gmail.com>
2019-02-12 10:32:15 +0100

    Grundidee

---
+++ Backrezepte/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Grundstein gelegt für das Backrezepte Buch
+        // nach und nach werden hier die Codezeilen geschrieben
+        System.out.println("Befindet sich noch in der Ideen Entwicklung");
+    }
+}
